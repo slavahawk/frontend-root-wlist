e6bdd30c2319d9d1bbf69b981120d0d14201d125
Slava <krylov_s10@mail.ru>
2024-12-13 11:35:29 +0700

    save theme/2

--- src/components/FloatingConfigurator.vue
+++ src/components/FloatingConfigurator.vue
@@ -6,7 +6,6 @@ const {  layoutConfig, toggleDarkMode, isDarkTheme } = useLayout();
 </script>
 
 <template>
-  {{layoutConfig}}
     <div class="fixed flex gap-4 top-8 right-8">
         <Button type="button" @click="toggleDarkMode" rounded :icon="isDarkTheme ? 'pi pi-moon' : 'pi pi-sun'" severity="secondary" />
         <div class="relative">
